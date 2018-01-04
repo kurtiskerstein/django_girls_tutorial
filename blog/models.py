03baf2f688e4588bd5f5e4da3dc6784bdc34eaf3
kurtiskerstein <krkerstein@gmail.com>
2018-01-04 14:08:05 -0600

    site working

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 # Create your models here.
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.user')
     title = models.CharField(max_length=200)
